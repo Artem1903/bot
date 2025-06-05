bdff829721088c28661a83c4e18f0e4c70a82375
Artem1903 <4388326@gmail.com>
2025-06-05 23:44:02 +0500

    Update send_to_admin.py

--- send_to_admin.py
+++ send_to_admin.py
@@ -1,13 +1,12 @@
-
-import os
 import httpx
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+TOKEN = "7601158787:AAE52sbM7kd6DfBWpXPnr0_Q1w4y9am5h9o"
 
-async def send_telegram_message(username, text):
+async def send_telegram_message(chat_id, text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     async with httpx.AsyncClient() as client:
-        await client.post(API_URL, json={
-            "chat_id": username,
-            "text": text
+        await client.post(url, json={
+            "chat_id": chat_id,
+            "text": text,
+            "parse_mode": "HTML"  # если нужно поддерживать жирный текст
         })
