1177c8835900a198676a2883b58cd9e9162c0867
Artem1903 <4388326@gmail.com>
2025-06-09 15:49:52 +0500

    Update state_manager.py

--- state_manager.py
+++ state_manager.py
@@ -1,13 +1,13 @@
 from state_manager import get_state, set_state
 
-...
+# ...
 
-    chat_id = from_number.replace("whatsapp:", "")
-    state = get_state(chat_id) or "start"
+chat_id = from_number.replace("whatsapp:", "")
+state = get_state(chat_id) or "start"
 
-    response = dialog_tree.get(state, {}).get("message", "Извините, я Вас не понял.")
-    next_state = dialog_tree.get(state, {}).get("next", {}).get(message_body)
+response = dialog_tree.get(state, {}).get("message", "Извините, я Вас не понял.")
+next_state = dialog_tree.get(state, {}).get("next", {}).get(message_body.strip())
 
-    if next_state:
-        set_state(chat_id, next_state)
-        response = dialog_tree.get(next_state, {}).get("message", response)
+if next_state:
+    set_state(chat_id, next_state)
+    response = dialog_tree.get(next_state, {}).get("message", response)
